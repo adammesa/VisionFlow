72d12ac107f6bee85f8d361b13e2d35b8342a1e2
muffinjello <muffinjello@outlook.com>
2013-03-22 11:23:41 -0700

    Buggy :(

--- src/me/muffinjello/visionflow/VisionFlow.java
+++ src/me/muffinjello/visionflow/VisionFlow.java
@@ -3,27 +3,22 @@ package me.muffinjello.visionflow;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class VisionFlow extends JavaPlugin {
+public class VisionFlow extends JavaPlugin implements Listener {
     //Startup
     @Override
     public void onEnable(){
         PluginDescriptionFile pdfFile = this.getDescription();
         this.getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " has been enabled!");
     }
-    @Override
-    public void onDisable(){
-        PluginDescriptionFile pdfFile = this.getDescription();
-        this.getLogger().info(pdfFile.getName() + " version " + pdfFile.getVersion() + " has been disabled!");
-    }
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
         if (command.getName().equalsIgnoreCase("vision")){
             if (sender.hasPermission("visionflow.vision")){
-                PluginDescriptionFile pdfFile = this.getDescription();
                 if (args.length >= 1){
                     String argtwo = args[0].trim();
                     if (argtwo.equalsIgnoreCase("on")){
@@ -32,6 +27,7 @@ public class VisionFlow extends JavaPlugin {
                         sender.sendMessage(ChatColor.YELLOW + "Turned vision " + ChatColor.GOLD + "on");
                     } else if(argtwo.equalsIgnoreCase("off")){
                         sender.sendMessage(ChatColor.YELLOW + "Turned vision " + ChatColor.RED + "off");
+                        sender.removePotionEffect(PotionEffectType.NIGHT_VISION);
                     }
                 } else {
                     sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
